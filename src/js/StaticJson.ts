a8994d879af9eae8c118c556f31673f398fd350f
Priyanka Logixal <priyanka.sharma@logixal.com>
2018-04-24 16:57:29 +0530

    Prepared JSON file for PLP page.

--- src/js/StaticJson.ts
+++ src/js/StaticJson.ts
@@ -1,5 +1,34 @@
 export default class StaticJson{
-    public productList = {
-        'aba':'daba'
-    }
+    public productList = 
+        [
+            {
+                name:'Adidas Outdoor Men's Caprock Trail Hiking Shoes',
+                listPrice :2499.00,
+                salePrice: :$1545.00-$1230.00  ,
+                Rating:'Stars'
+            },
+            {
+                name:'Nike Men's Air Force 1 Running Shoes',
+                listPrice :$2371.99-$2399.99,
+                Rating:'Stars'
+            },
+            {
+                name:'Adidas Outdoor Men's Caprock Trail Hiking Shoes',
+                listPrice :2499.00,
+                salePrice: :$1545.00-$1230.00  ,
+                Rating:'Stars'
+            },
+            {
+                name:'Nike Men's Air Force 1 Running Shoes',
+                listPrice :$2371.99-$2399.99,
+                Rating:'Stars'
+            },
+            {
+                name:'Adidas Outdoor Men's Caprock Trail Hiking Shoes',
+                listPrice :2499.00,
+                salePrice: :$1545.00-$1230.00  ,
+                Rating:'Stars'
+            }
+        ]
+    
 }
