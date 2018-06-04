e485d51610647fd62415795a9942c7b2c248b61d
KD <koustubh.desai@logixal.com>
2018-06-04 21:11:59 +0530

    Final changes 5

--- dist/js/ts_compiled/oracle/Utils/StaticJson.js
+++ dist/js/ts_compiled/oracle/Utils/StaticJson.js
@@ -26,7 +26,7 @@ define(["require", "exports"], function (require, exports) {
                         { src: "couch_sm_1.jpg", alt: "Sofa view 2" },
                         { src: "couch_sm_0.jpg", alt: "Sofa view 3" },
                     ],
-                    colors: ['red', 'green', 'blue'],
+                    colors: ['white', 'grey', 'blue'],
                     sizes: ['small', 'medium', 'large', 'xtra large'],
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                 }
@@ -55,7 +55,7 @@ define(["require", "exports"], function (require, exports) {
                         { src: "couch_sm_1.jpg", alt: "Sofa view 2" },
                         { src: "couch_sm_0.jpg", alt: "Sofa view 3" },
                     ],
-                    colors: ['red', 'green', 'blue'],
+                    colors: ['bluee', 'slategray', 'electro'],
                     sizes: ['small', 'medium', 'large', 'xtra large'],
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                 }
@@ -84,7 +84,7 @@ define(["require", "exports"], function (require, exports) {
                         { src: "couch_sm_1.jpg", alt: "Sofa view 2" },
                         { src: "couch_sm_0.jpg", alt: "Sofa view 3" },
                     ],
-                    colors: ['red', 'green', 'blue'],
+                    colors: ['electro', 'bluee', 'white'],
                     sizes: ['small', 'medium', 'large', 'xtra large'],
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                 }
--- src/js/oracle/Utils/StaticJson.ts
+++ src/js/oracle/Utils/StaticJson.ts
@@ -24,7 +24,7 @@ const parentJson = {
                     {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
                     {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
-                colors:['red','green','blue'],
+                colors:['white','grey','blue'],
                 sizes:['small','medium','large','xtra large'],
                 description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             }            
@@ -53,7 +53,7 @@ const parentJson = {
                     {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
                     {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
-                colors:['red','green','blue'],
+                colors:['bluee','slategray','electro'],
                 sizes:['small','medium','large','xtra large'],
                 description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             } 
@@ -82,7 +82,7 @@ const parentJson = {
                     {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
                     {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
-                colors:['red','green','blue'],
+                colors:['electro','bluee','white'],
                 sizes:['small','medium','large','xtra large'],
                 description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             } 
