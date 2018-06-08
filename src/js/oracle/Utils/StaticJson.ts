b518e203924c347344bd899ef26c05a808bbcc94
KD <koustubh.desai@logixal.com>
2018-06-08 21:52:04 +0530

    fixing json

--- src/js/oracle/Utils/StaticJson.ts
+++ src/js/oracle/Utils/StaticJson.ts
@@ -20,9 +20,9 @@ const parentJson = {
             details:{
                 title:'Product Title 1',
                 images:[
-                    {src:"adidas_3.png",alt:"Shoes view 1"},
-                    {src:"adidas_1.jpg",alt:"Shoes view 2"},
-                    {src:"adidas_3.png",alt:"Shoes view 3"},
+                    {src:"couch_lg_0.jpg",alt:"Sofa view 1"},
+                    {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
+                    {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
                 colors:['white','grey','blue'],
                 sizes:['small','medium','large','xtra large'],
@@ -30,7 +30,7 @@ const parentJson = {
             }            
         },
         {
-            id:"5",
+            id:"2",
             name:"Nike Men's Air Force 1 Running Shoes",
             image:{
                 src:"product-image_1.png",
@@ -49,11 +49,11 @@ const parentJson = {
             details:{
                 title:'Product Title 2',
                 images:[
-                    {src:"adidas_3.png",alt:"Shoes view 1"},
-                    {src:"adidas_1.jpg",alt:"Shoes view 2"},
-                    {src:"adidas_3.png",alt:"Shoes view 3"},
+                    {src:"couch_lg_0.jpg",alt:"Sofa view 1"},
+                    {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
+                    {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
-                colors:['green','slategray','electro'],
+                colors:['light blue','slategray','electro'],
                 sizes:['small','medium','large','xtra large'],
                 description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             } 
@@ -78,11 +78,11 @@ const parentJson = {
             details:{
                 title:'Product Title 2',
                 images:[
-                    {src:"adidas_3.png",alt:"Shoes view 1"},
-                    {src:"adidas_1.jpg",alt:"Shoes view 2"},
-                    {src:"adidas_3.png",alt:"Shoes view 3"},
+                    {src:"couch_lg_0.jpg",alt:"Sofa view 1"},
+                    {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
+                    {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
-                colors:['electro','light blue','white', 'orange'],
+                colors:['electro','light blue','white'],
                 sizes:['small','medium','large','xtra large'],
                 description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             } 
@@ -107,9 +107,9 @@ const parentJson = {
             details:{
                 title:'Product Title 2',
                 images:[
-                    {src:"adidas_3.png",alt:"Shoes view 1"},
-                    {src:"adidas_1.jpg",alt:"Shoes view 2"},
-                    {src:"adidas_3.png",alt:"Shoes view 3"},
+                    {src:"couch_lg_0.jpg",alt:"Sofa view 1"},
+                    {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
+                    {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
                 colors:['white','grey','blue','light blue','slategray' ,'electro'],
                 sizes:['small','medium','large','xtra large'],
@@ -117,35 +117,7 @@ const parentJson = {
             } 
         },
         {
-            id:"5",
-            name:"Adidas Outdoor Men's Caprock Trail Hiking Shoes",
-            image:{
-                src:"product-image_0.png",
-                alt:"Hiking shoes"
-            },
-            brand:"adidas",
-            price:{
-                currency:"$",
-                sellingPrice:"2499.00",
-                discountedPrice:"2960.99",
-                sale:true,
-                low:"1547.00",
-                high:"1230.00"
-            },
-            rating:1,
-            details:{
-                title:'Product Title 2',
-                images:[
-                    {src:"adidas_3.png",alt:"Shoes view 1"},
-                    {src:"adidas_1.jpg",alt:"Shoes view 2"},
-                    {src:"adidas_2.jpg",alt:"Shoes view 3"},
-                ],
-                colors:['electro','light blue','white'],
-                sizes:['small','medium','large','xtra large'],
-                description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-            }, 
-        {
-            id:"6",
+            id:"4",
             name:"Nike Men's Air Force 1 Running Shoes",
             image:{
                 src:"product-image_1.png",
@@ -157,18 +129,47 @@ const parentJson = {
                 sellingPrice:"2499.00",
                 discountedPrice:"2960.99",
                 sale:true,
-                low:"1546.00",
+                low:"1548.00",
                 high:"1230.00"
             },
-            rating:4,
+            rating:2,
             details:{
                 title:'Product Title 2',
                 images:[
-                    {src:"adidas_3.png",alt:"Shoes view 1"},
-                    {src:"adidas_1.jpg",alt:"Shoes view 2"},
-                    {src:"adidas_3.png",alt:"Shoes view 3"},
+                    {src:"couch_lg_0.jpg",alt:"Sofa view 1"},
+                    {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
+                    {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
                 ],
-                colors:['light blue','slategray','electro'],
+                colors:['white','grey','blue','light blue','slategray' ,'electro'],
+                sizes:['small','medium','large','xtra large'],
+                description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            } 
+        },
+        {
+            id:"4",
+            name:"Nike Men's Air Force 1 Running Shoes",
+            image:{
+                src:"product-image_1.png",
+                alt:"Hiking shoes"
+            },
+            brand:"360 degree",
+            price:{
+                currency:"$",
+                sellingPrice:"2499.00",
+                discountedPrice:"2960.99",
+                sale:true,
+                low:"1548.00",
+                high:"1230.00"
+            },
+            rating:2,
+            details:{
+                title:'Product Title 2',
+                images:[
+                    {src:"couch_lg_0.jpg",alt:"Sofa view 1"},
+                    {src:"couch_sm_1.jpg",alt:"Sofa view 2"},
+                    {src:"couch_sm_0.jpg",alt:"Sofa view 3"},
+                ],
+                colors:['white','grey','blue','light blue','slategray' ,'electro'],
                 sizes:['small','medium','large','xtra large'],
                 description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             } 
@@ -177,14 +178,6 @@ const parentJson = {
     colorCodes:[
         {
             name:'white',
-            code:'46df6a '
-        },
-		        {
-            name:'green',
-            code:'dcdde1'
-        },
-		        {
-            name:'orange',
             code:'dcdde1'
         },
         {
